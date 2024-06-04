044cd340ecc099590d67a6c549961f41997038ce
karamany1998 <93926604+karamany1998@users.noreply.github.com>
2024-06-04 16:18:35 +0100

    Working on function to convert samples to LP-coefficients, will make a class when this function works

--- LPC-Vocoder/target/example.cpp
+++ LPC-Vocoder/target/example.cpp
@@ -5,7 +5,15 @@
 #include "wavEinlesen.h"
 #include "Write.h"
 #include "wavWrite.h"
+#include<valarray> 
 
+extern "C"
+{
+#include "FIR.h"
+#include "speech2Lsf.h"
+#include "signalUtils.h"
+#include "lsflpcUtils.h"
+}
 
 
 using namespace std;
@@ -78,81 +86,117 @@ void MileStone4()
 	wavWriterObject.convertPCMToWav(pcmName);
 }
 
-/*
-//a function that converts a pcm file to a wav file 
-void convertPCMToWAV()
-{
-	string pcmName;
-	cout << "please insert the name for the pcm file to read " << endl;
-	cin >> pcmName;
-	Einlesen pcmFile(pcmName);
-	pcmFile.readFile();
 
-	string wavName; 
-	cout << "please enter the name for the wav file" << endl;
+
+
+
+//this function is used to convert a set of audio samples(divided into frames of size N=160) to LPC coefficients
+void convertSamplesToLPC()
+{
+
+
+	//first we have to read either a PCM or Wav File
+	//for this function,  we will read a wav file.
+
+	
+	cout << "please enter the wav file you want to read to determine the LP-Coefficients" << endl;
+	string wavName;
 	cin >> wavName;
-	FILE* pcm2wav_handle = fopen((wavName+".wav").c_str(), "wb");
+	wavEinlesen readerObj(wavName);
+	readerObj.readFile();
+	vector<vector<short>>& currVec = readerObj.audioRahmen; 
+	vector<valarray<short>> valArr;
+
+
+
+
+	//go over the vector<vector<short>> of audio frames and convert it to valrray for ease of computation
+	for (int i = 0; i < currVec.size(); i++)		
+	{
+		valarray<short> cpy(  currVec[i].data(), currVec[i].size()  );
+		valArr.push_back(cpy);
+	}
+
+
+	vector<valarray<double>> LSF_vec; 
+
+	//construct the hamming function(will be used later to computer Line Spectral Frequencies)
+	double arrayWindowSpeech[10 + 160 + 80];	
+	for (int idx = 0; idx < 250; idx++)
+	{	
+		if (idx == 240)	//used for debugging
+		{
+			arrayWindowSpeech[idx] = hammingWindow(idx, 125, 10 + 160 + 80);
+		}
+		else
+		{
+			arrayWindowSpeech[idx] = hammingWindow(idx, 125, 10 + 160 + 80);
+		}
+		
+
+	}
 	
 
-	vector<vector<short>>& audioFrame = pcmFile.audioRahmen;
-
-
-	char* riff =  "RIFF" ;
-	char* wav = "WAVE";
-	char* str3 = "fmt ";
-	char* data = "data";
-
-	cout << "length of RIFF char* is " << strlen(riff) << endl;
-
-
-	string test;
-	cin >> test;
-	unsigned int sizePCM = audioFrame.size() * 160 * 2;  //size is how many frames*size of each frame * 2 bytes
-	unsigned int lenFormat = 0x10;
-	short anotherInt = 0x01;
-	short numChannels = 1;
-	unsigned int sampleRate = 8000;
-	unsigned int bytesPerSecond = 16000;
-
-	short bytesPerSample = 2; 
-	short bitsPerSample = 16;
-
-	unsigned int totalLengthBytes = sizePCM + (44-8); 
-	unsigned int fileLength = sizePCM;
-
-
-	//Write the header file for the wave file at the beginning
-
-	fwrite(riff, 1 , strlen(riff), pcm2wav_handle);	//write "RIFF" to wavFile
-	fwrite(&totalLengthBytes, 4, 1, pcm2wav_handle);		//write fileSize to wavFile
-	fwrite(wav, strlen(wav), 1, pcm2wav_handle); //write "WAVE" to file
-	fwrite(str3, strlen(str3), 1, pcm2wav_handle); // write "fmt␣" to file
-	fwrite(&lenFormat, 4, 1, pcm2wav_handle);
-	fwrite(&anotherInt, 2, 1, pcm2wav_handle);
-	fwrite(&numChannels, 2, 1, pcm2wav_handle);
-	fwrite(&sampleRate, 4, 1, pcm2wav_handle);
-	fwrite(&bytesPerSecond, 4, 1, pcm2wav_handle);
-	fwrite(&bytesPerSample, 2, 1, pcm2wav_handle);
-	fwrite(&bitsPerSample, 2, 1, pcm2wav_handle);
-	fwrite(data, strlen(data), 1, pcm2wav_handle);
-	fwrite(&fileLength, 4, 1, pcm2wav_handle);
-
-
-	//after the header, write the pcm file samples to the output file
-	//write pcm data to file
-	for (int i = 0; i < audioFrame.size(); i++)
+	//now, we can loop over the vector of valarrays and then get the 10 lookback samples, 80 lockahead samples and current samples(will equal 160)
+	//then combine into one vec/valarray of size N = 250
+	//after that we can use the given functions to convert the samples to line spectral frequencies and then to LPC 
+	for (int i = 0; i < valArr.size(); i++)
 	{
-		int num = fwrite( &audioFrame[i][0] , sizeof(short), audioFrame[i].size(), pcm2wav_handle);
+		int n = valArr.size();
+		valarray<short> lookBackVec(valArr[i][0] , 10); // default set to first value
+		valarray<short> lookAheadVec(valArr[i][159] , 80);  //default set to last value
+		vector<double> combinedSamples; 
+
+
+		if (i != 0 )	//we can get both 10 lookBack samples 
+		{
+			lookBackVec = valArr[i - 1][slice(160 - 11, 10, 1)];		
+		}
+		if (i != valArr.size() - 1)
+		{
+			lookAheadVec = valArr[i + 1][slice(160 - 80 - 1, 80, 1)];
+		}
+		
+		for (int j = 0; j < lookBackVec.size(); j++)combinedSamples.push_back(lookBackVec[j]);
+		for (int j = 0; j < valArr[i].size(); j++)combinedSamples.push_back(valArr[i][j]);
+		for (int j = 0; j < lookAheadVec.size(); j++)combinedSamples.push_back(lookAheadVec[j]);
+
+
+		//now combined samples has 10+160 + 80 samples
+		//we can now use speech2lsp_array
+		double outputLSF[10];
+		double outputLPC[10];
+	
+		double arrayWindowCorrelation[10];
+		for (int k = 0; k < 10; k++)arrayWindowCorrelation[k] = 1;
+
+		
+		speech2lsp_array(combinedSamples.data(), outputLSF, 250, 10, arrayWindowSpeech , arrayWindowCorrelation);
+		convertLsf2Lpc(outputLSF, outputLPC, 10);
+
+		cout << "LPC Coefficients for i = " <<i<<" are "<< endl;
+
+
+		for (int k = 0; k < 10; k++)
+		{
+			cout << outputLPC[k] << " ";
+		}
+
+		cout << endl;
+		
+
+
+
 		
 	}
 
-	cout << "Conversion from PCM to Wav is done...." << endl;
-	cout << "=============================================================================================" << endl;
-	cout << "=============================================================================================" << endl;
-	fclose(pcm2wav_handle);
+	
+
+
+
+
 
 }
-*/
 
 
 int main()
@@ -162,7 +206,9 @@ int main()
 	//test2();
 	//test3();
 
-	MileStone4();
+	//MileStone4();
+
+	convertSamplesToLPC();
 
 
 	
