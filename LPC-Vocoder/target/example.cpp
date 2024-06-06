0a2a43628071e93468f15c525a4656d147967b0b
karamany1998 <93926604+karamany1998@users.noreply.github.com>
2024-06-06 12:11:32 +0100

    meilstein6 main functionality is in the function - determineRestSignal()

--- LPC-Vocoder/target/example.cpp
+++ LPC-Vocoder/target/example.cpp
@@ -15,6 +15,7 @@ extern "C"
 #include "speech2Lsf.h"
 #include "signalUtils.h"
 #include "lsflpcUtils.h"
+
 }
 
 
@@ -106,116 +107,73 @@ void MileStone5()	//will create a class to do LP Analysis by putting the functio
 
 
 
-//this function is used to convert a set of audio samples(divided into frames of size N=160) to LPC coefficients
-void convertSamplesToLPC()
+void determineRestSignal()
 {
-
-
-	
-	//for this function,  we will read a wav file.
-
+	//Initially we will use the same functionality as milestone5, but then we the LPC-coefficients to filter the signal
 	cout << "please enter the wav file you want to read to determine the LP-Coefficients" << endl;
 	string wavName;
 	cin >> wavName;
 	wavEinlesen readerObj(wavName);
 	readerObj.readFile();
-
-	vector<vector<short>>& currVec = readerObj.audioRahmen; //the current audio frames
-
-	vector<valarray<short>> valArr;	
-
-
-
-
-	//go over the vector<vector<short>> of audio frames and convert it to valrray for ease of computation
-	for (int i = 0; i < currVec.size(); i++)		
-	{
-		valarray<short> cpy(  currVec[i].data(), currVec[i].size()  );
-		valArr.push_back(cpy);
-	}
-
-
-	vector<valarray<double>> LPC_vec;	//a vector of valarrays where each valarray has the 11 LPC Coefficients
-
-
-	//construct the hamming function(will be used later to computer Line Spectral Frequencies)
-	double arrayWindowSpeech[10 + 160 + 80];	
-	for (int idx = 0; idx < 250; idx++)
-	{	
-		arrayWindowSpeech[idx] = hammingWindow(idx, 125, 250);
-	}
-
-	double arrayWindowCorrelation[11];
-	for (int k = 0; k < 11; k++)arrayWindowCorrelation[k] = 1.0;
-	
-
-	//now, we can loop over the vector of valarrays and then get the 10 lookback samples, 80 lockahead samples and current samples(will equal 160)
-	//then combine into one vec/valarray of size N = 250
-	//after that we can use the given functions to convert the samples to line spectral frequencies and then to LPC 
-	for (int i = 0; i < valArr.size(); i++)
-	{
-		int n = valArr.size();
-		valarray<short> lookBackVec(valArr[i][0] , 10); // default set to first value
-		valarray<short> lookAheadVec(valArr[i][159] , 80);  //default set to last value
-
-		vector<double> combinedSamples; 
-
-
-		
-		if (i >0 )	//we can get 10 lookBack samples 
-		{
-			int sz = valArr[i - 1].size();
-			lookBackVec = valArr[i - 1][slice(sz-10 , 10 , 1)];		
-		}
-		if (i < valArr.size() - 1)	//we can get 80 lookAhead samples
-		{
-			int sz = valArr[i + 1].size();
-			lookAheadVec = valArr[i + 1][slice(0, 80, 1)];
-		}
-		
-		
-
-		//now combined samples has 10+160 + 80 samples
-		for (int j = 0; j < lookBackVec.size(); j++)combinedSamples.push_back(lookBackVec[j]);
-		for (int j = 0; j < valArr[i].size(); j++)combinedSamples.push_back(valArr[i][j]);
-		for (int j = 0; j < lookAheadVec.size(); j++)combinedSamples.push_back(lookAheadVec[j]);
-		
-		
-		
-		//we can now use speech2lsp_array  and convertLsf2Lpc to get the LPC- coefficients
-		double outputLSF[10]; //has the line spectral frequencies
-		double outputLPC[11]; //has the 11 LPC Coefficients
-	
-		
-		speech2lsp_array(combinedSamples.data(), outputLSF, 250, 10, arrayWindowSpeech , arrayWindowCorrelation);
-		convertLsf2Lpc(outputLSF, outputLPC, 10);
-
-		
-		valarray<double> lpcCurr(outputLPC, 11);
-		LPC_vec.push_back(lpcCurr);
-
-		
-	}
-
+	vector<vector<short>>& audioFrames = readerObj.audioRahmen;
+	vector<vector<double>> audioFramesD;
 
 	
-	cout << "========================================================================" << endl;
-	cout << "The LPC Coefficients computed are :" << endl;
+	
+	
+	LPAnalysis LPC_obj(readerObj);
+	LPC_obj.convertSamplesToLPC();
 
-	for (int i = 0; i < LPC_vec.size(); i++)
+	vector<valarray<double>>& LPC_coefficients = LPC_obj.LPC_Coefficients;
+	double initalStates[11];
+	double LP_arr[11];
+
+
+	for (int j = 0; j < 11; j++)
+		LP_arr[j] = LPC_coefficients[0][j];
+
+	for (int i = 0; i < 11; i++)	//initial states will be set initially to 0
 	{
-		cout << i << ": ";
-		for (int j = 0; j < LPC_vec[i].size(); j++)
+		initalStates[i] = 0;
+	}
+	
+	vector<vector<double>> restSignal;
+
+	for (int i = 0; i < audioFrames.size(); i++)
+	{
+
+		std::vector<double> currentFrame(audioFrames[i].begin(), audioFrames[i].end());
+		double outputBuffer[160];
+		double currentCoefficient[11];
+		for (int j = 0; j < 11; j++)currentCoefficient[j] = LPC_coefficients[i][j];
+
+		InitFIR(initalStates, currentCoefficient , 11);
+		ExecuteFIRDF2(currentFrame.data(), outputBuffer, audioFrames[i].size());
+		vector<double> curr(outputBuffer , outputBuffer+160);
+		restSignal.push_back(curr);
+
+		ReturnStatesFIR(initalStates); //save the internal states to the initialStates array
+
+		TerminateFIR(); //terminate FIR-filter and iterate to the next audio frame with the modified states
+	
+	}
+
+	 
+	cout << "Filtering the input signal is over..." << endl;
+	cout << "press any key to continue " << endl;
+	cout << "=============================================================" << endl;
+	cout << "The rest signal after filtering is:: " << endl;
+
+	string x; cin >> x;
+	
+	for (int i = 0; i < restSignal.size(); i++)
+	{
+		for (int j = 0; j < restSignal[i].size(); j++)
 		{
-			cout << LPC_vec[i][j] << " ";
+			cout << restSignal[i][j] << " ";
 		}
 		cout << endl;
 	}
-	
-
-
-	
-
 
 }
 
@@ -231,8 +189,9 @@ int main()
 	//convertSamplesToLPC();
 
 
-	MileStone5();
+	//MileStone5();
 
+	determineRestSignal();
 
 	
 
